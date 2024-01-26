5b00d8863c8445c2081159c257a052756c1af031
MarGama <210358@student.glu.nl>
2024-01-26 21:26:56 +0100

    camewa

---
+++ MakeMeLaugh/Assets/Scripts/MultiplayerCameraUpdater.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MultiplayerCameraUpdater : MonoBehaviour
+{
+    CinemachineTargetGroup targetGroup;
+
+    private void Awake()
+    {
+        targetGroup = FindObjectOfType<CinemachineTargetGroup>();
+    }
+
+    public void AddPlayerToGroup(PlayerInput player)
+    {
+        targetGroup.AddMember(player.transform, 1, 1);
+    }
+
+    public void RemovePlayerFromGroup(PlayerInput player)
+    {
+        targetGroup.RemoveMember(player.transform);
+    }
+}
