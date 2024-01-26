5261f8a9b440a94bcaff705ed85a63d4b8a02361
MarGama <210358@student.glu.nl>
2024-01-26 11:00:42 +0100

    added leantween

--- MakeMeLaugh/Assets/Scripts/Character/IKFootStepper.cs
+++ MakeMeLaugh/Assets/Scripts/Character/IKFootStepper.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class IKFootStepper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform leftCastPoint, rightCastPoint, leftFoot, rightFoot;
+    [SerializeField] private float stepTriggerDistance, stepSpeed, stepHeight;
+    private Coroutine rightStepRoutine, leftStepRoutine;
+
+    private void Update()
     {
+        //determine if feet are too far away
+        //if outside radius, step foot towards new location
+    }
+
+    private void CheckFoot(Transform castPoint, Transform target)
+    {
+        //check distance
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator StepRoutine(Transform target, Vector3 targetPoint, Coroutine routine)
     {
-        
+        Vector3 startPoint = target.position;
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime / stepSpeed;
+            target.position = GetStepInterpolation(startPoint, targetPoint, t);
+
+            yield return null;
+        }
+
+        target.position = GetStepInterpolation(startPoint, targetPoint, 1);
+        routine = null;
+    }
+
+    private Vector3 GetStepInterpolation(Vector3 start, Vector3 end, float percent)
+    {
+        Vector3 result = new Vector3();
+        result.x = LeanTween.easeInOutQuart(start.x, end.x, percent);
+        result.z = LeanTween.easeInOutQuart(start.z, end.z, percent);
+        result.y = Mathf.Sin(percent * Mathf.PI) * stepHeight;
+        return result;
     }
 }
