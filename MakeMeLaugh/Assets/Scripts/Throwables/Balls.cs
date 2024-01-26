f459d77f294be845b647e38eed65c725cb4d7c05
MarGama <210358@student.glu.nl>
2024-01-26 23:49:20 +0100

    some yeeting

---
+++ MakeMeLaugh/Assets/Scripts/Throwables/Balls.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour, IThrowable
+{
+    private bool isActive = false;
+    [SerializeField] private float yeetForce;
+
+    public void Throw()
+    {
+        isActive = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Vector3 dir = (collision.transform.position - transform.position).normalized;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(dir * yeetForce, ForceMode.Impulse);
+            Destroy(this);
+        }
+    }
+}
