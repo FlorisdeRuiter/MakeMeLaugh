68118781a94a0b4b01cc0a163a3aef468596998a
MarGama <210358@student.glu.nl>
2024-01-27 12:40:43 +0100

    Update EnemyHealth.cs

--- MakeMeLaugh/Assets/Scripts/Enemy/EnemyHealth.cs
+++ MakeMeLaugh/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [SerializeField] UnityEvent deathEvent;
+    private Animator animator;
+    bool isAlive = true;
+
     public void DamageEnemy()
     {
+        isAlive = false;
         GetComponent<EnemyBehaviour>().enabled = false;
         Invoke("Disappear", 3);
+        animator.SetTrigger("death");
         DodgeballManager.instance.RemovePeasant(gameObject);
+        deathEvent.Invoke();
     }
 
-    private void Disappear()
+    public void Disappear()
     {
         Destroy(gameObject);
     }
