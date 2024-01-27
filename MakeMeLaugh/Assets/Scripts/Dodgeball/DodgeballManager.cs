b6b65a068bf0630115ab3bb18d9e4e5ad20476a7
MarGama <210358@student.glu.nl>
2024-01-27 07:12:00 +0100

    DODGEBALL

---
+++ MakeMeLaugh/Assets/Scripts/Dodgeball/DodgeballManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeballManager : MonoBehaviour
+{
+    public static DodgeballManager instance;
+
+    [SerializeField] private float peasantsPerSide;
+    private List<GameObject> p1Peasants, p2Peasants;
+    [SerializeField] private Transform p1Min, p1Max, p2Min, p2Max;
+    private GameObject p1PeasantPrefab, p2PeasantPrefab;
+
+    [SerializeField] private float timeToHoldBall = 3;
+    [SerializeField] private Transform ballAreaMin, ballAreaMax;
+
+    private void Awake()
+    {
+        instance = this;
+        p1Peasants = new List<GameObject>();
+        p2Peasants = new List<GameObject>();
+    }
+
+    private void SpawnPeasants()
+    {
+        for (int i = 0; i < peasantsPerSide; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(p1Min.position.x, p1Max.position.x), Random.Range(p1Min.position.y, p1Max.position.y), Random.Range(p1Min.position.z, p1Max.position.z));
+            GameObject peasant = Instantiate(p1PeasantPrefab, position, Quaternion.identity);
+            p1Peasants.Add(peasant);
+        }
+
+        for (int i = 0; i < peasantsPerSide; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(p2Min.position.x, p2Max.position.x), Random.Range(p2Min.position.y, p2Max.position.y), Random.Range(p2Min.position.z, p2Max.position.z));
+            GameObject peasant = Instantiate(p2PeasantPrefab, position, Quaternion.identity);
+            p2Peasants.Add(peasant);
+        }
+    }
+
+    private void RemovePeasant(GameObject peasant)
+    {
+        if (p1Peasants.Contains(peasant))
+        {
+            p1Peasants.Remove(peasant);
+            if (p1Peasants.Count <= 0)
+            {
+                //p2 wins
+            }
+        }
+        if (p2Peasants.Contains(peasant))
+        {
+            p2Peasants.Remove(peasant);
+            if (p2Peasants.Count <= 0)
+            {
+                //p1 wins
+            }
+        }
+    }
+
+    private void HoldBallOutOfPlay(GameObject ball)
+    {
+        StartCoroutine(HoldBalls(ball));
+    }
+
+    private IEnumerator HoldBalls(GameObject ball)
+    {
+        float t = 0f;
+        while (t < timeToHoldBall)
+        {
+            yield return null;
+            t += Time.deltaTime;
+        }
+        ball.transform.position = new Vector3(Random.Range(ballAreaMin.position.x, ballAreaMax.position.x), Random.Range(ballAreaMin.position.y, ballAreaMax.position.y), Random.Range(ballAreaMin.position.z, ballAreaMax.position.z));
+        Rigidbody rBody = ball.GetComponent<Rigidbody>();
+        rBody.velocity = Vector3.zero;
+        rBody.angularVelocity = Vector3.zero;
+    }
+
+    private IEnumerator RunCountdown()
+    {
+        SpawnPeasants();
+
+        float t = 3;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            ShowCountdown((int)Mathf.Round(t));
+            yield return null;
+        }
+    }
+
+    private void ShowCountdown(int number)
+    {
+        //send to uimanager, make it visual
+    }
+}
